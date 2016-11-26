e1b489e5a5e7e7ebc93feec26c7901883a98e1f2
aozora <marcello.palmitessa@gmail.com>
2016-11-26 23:10:45 +0100

    wip webpack config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,39 @@
 import Vue from 'vue';
-import App from './App';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import Navigation from 'components/Navigation/navigation';
+import Loader from 'components/Loader/Loader';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  // template: '<App/>',
-  components: { App },
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+import routes from 'src/routes';
+import 'src/assets/scss/app.scss';
+
+export const LoadingState = new Vue();
+
+export const router = new VueRouter({
+  routes,
+  mode: 'history',
+  linkActiveClass: 'active'
 });
+
+export const App = new Vue({
+  router,
+  components: {
+    Navigation,
+    Loader
+  },
+
+  data(){
+    return {
+      isLoading: false
+    };
+  },
+
+  created(){
+    LoadingState.$on('toggle', (isLoading) => {
+      this.isLoading = isLoading;
+    });
+  }
+}).$mount('#app');
